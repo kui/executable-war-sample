01042949292273f6067cb3c2e7641397aaac2635
Keiichiro Ui <keiichiro.ui@gmail.com>
2013-05-09 22:14:56 +0900

    fix for warnning

--- jetty/src/main/java/jp/k_ui/sample/Main.java
+++ jetty/src/main/java/jp/k_ui/sample/Main.java
@@ -8,15 +8,14 @@ import org.eclipse.jetty.webapp.WebAppContext;
 public class Main {
     public static void main(String[] args) throws Exception {
         int port = Integer.parseInt(System.getProperty("port", "8888"));
-        Server server = new Server(port);
-
-        WebAppContext webapp = new WebAppContext();
         URL warLocation = Main.class.getProtectionDomain().getCodeSource()
                 .getLocation();
-        System.out.println(warLocation);
+
+        WebAppContext webapp = new WebAppContext();
         webapp.setWar(warLocation.toExternalForm());
         webapp.setContextPath("/");
 
+        Server server = new Server(port);
         server.setHandler(webapp);
         server.start();
         server.join();
